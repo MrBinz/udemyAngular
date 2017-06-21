adf3f7e757aca19d2036d866c7b81ccddb0d8d4e
Fabio Neukirchen <fabio.neukirchen@adp.com>
2017-06-21 19:07:51 -0300

    First Examples and Exercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
 
-import { AppComponent } from './app.component';
+import {AppComponent} from "./app.component";
+import {ServerComponent} from "./server/server.component";
+import {ServersComponent} from "./servers/servers.component";
+import {SuccessComponent} from './alerts/success/success.component';
+import {WarningComponent} from './alerts/warning/warning.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessComponent,
+    WarningComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
